bed8ad0d542dea7ce020b382840a963e28980832
joshuaahanry <132141966+joshuaahanry@users.noreply.github.com>
2024-04-22 21:31:34 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+   <script>
+        var avatar = document.getElementById('avatar');
+        var mypic = document.getElementById('mypic');
+        var jumpscare = document.getElementById('jumpscare');
+
+        avatar.addEventListener('click', function() {
+            mypic.style.display = 'none';
+            jumpscare.style.display = 'block';
+        });
+
+        jumpscare.addEventListener('click', function() {
+            mypic.style.display = 'block';
+            jumpscare.style.display = 'none';
+        });
+
+        mypic.addEventListener('click', function() {
+            mypic.src = 'img/catsss.jpeg'; 
+        });
+    </script>
